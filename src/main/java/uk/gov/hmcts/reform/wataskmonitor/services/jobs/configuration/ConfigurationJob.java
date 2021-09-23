c1ded4bd1698b57b6ad8f5426122985ca346926e
Satvinder Hullait <1583258+satvinder-hullait@users.noreply.github.com>
2021-09-23 10:28:12 +0100

    RWA-714 [SH] Spike to add custom event to application insights

--- src/main/java/uk/gov/hmcts/reform/wataskmonitor/services/jobs/configuration/ConfigurationJob.java
+++ src/main/java/uk/gov/hmcts/reform/wataskmonitor/services/jobs/configuration/ConfigurationJob.java
@@ -1,14 +1,20 @@
 package uk.gov.hmcts.reform.wataskmonitor.services.jobs.configuration;
 
+import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.telemetry.EventTelemetry;
+import com.microsoft.applicationinsights.telemetry.MetricTelemetry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.wataskmonitor.domain.camunda.CamundaTask;
+import uk.gov.hmcts.reform.wataskmonitor.domain.jobs.GenericJobOutcome;
 import uk.gov.hmcts.reform.wataskmonitor.domain.jobs.GenericJobReport;
 import uk.gov.hmcts.reform.wataskmonitor.domain.taskmonitor.JobName;
 import uk.gov.hmcts.reform.wataskmonitor.services.JobService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static uk.gov.hmcts.reform.wataskmonitor.domain.taskmonitor.JobName.CONFIGURATION;
 import static uk.gov.hmcts.reform.wataskmonitor.utils.LoggingUtility.logPrettyPrint;
@@ -16,6 +22,8 @@ import static uk.gov.hmcts.reform.wataskmonitor.utils.LoggingUtility.logPrettyPr
 @Slf4j
 @Component
 public class ConfigurationJob implements JobService {
+
+    static final TelemetryClient telemetryClient = new TelemetryClient();
     private final ConfigurationJobService configurationJobService;
 
     @Autowired
@@ -33,6 +41,15 @@ public class ConfigurationJob implements JobService {
         log.info("Starting {} job.", CONFIGURATION);
         List<CamundaTask> tasks = configurationJobService.getUnConfiguredTasks(serviceToken);
         GenericJobReport report = configurationJobService.configureTasks(tasks, serviceToken);
+
+        Map<String,String> props = new HashMap<>();
+        for(GenericJobOutcome gjo : report.getOutcomeList()){
+            if(!gjo.isSuccessful()){
+                props.put("taskId", gjo.getTaskId());
+            }
+        }
+        
+        telemetryClient.trackEvent("FailingTasks", props, null);
         log.info("{} job finished successfully: {}", CONFIGURATION, logPrettyPrint(report));
     }
 }
